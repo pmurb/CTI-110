ea1059f23e0da720b04943b460dd8af4f5a3be95
pmurb <43518489+pmurb@users.noreply.github.com>
2018-09-23 10:48:06 -0400

    Add files via upload
    First tutorial

---
+++ MyFirstPythonProgram_PrestonUrbanek.py
@@ -0,0 +1,10 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print('#This is to demo python program'
+	  '#09/09/2018'
+	  '#CTI-110 Tutorial 1 - My First Python Program'
+	  '#Preston Urbanek')
+#This is to demo python program#09/09/2018#CTI-110 Tutorial 1 - My First Python Program#Preston Urbanek
+>>> print('This is a test of IDLE')
+This is a test of IDLE
+>>> 
